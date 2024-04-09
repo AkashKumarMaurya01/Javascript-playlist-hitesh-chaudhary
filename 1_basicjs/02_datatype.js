31c1363fcc4404799e7c96c8ce719c38f271eda2
Akash kumar maurya <akashmaurya09994@gmail.com>
2024-04-09 18:17:23 +0530

    data types

---
+++ 1_basicjs/02_datatype.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all Js code as newer version
+
+// alert("hello")  this will give error bcz we are using node not browser in the browser it run with pop alert notification
+
+//  data types 
+
+let age = 23
+let name = "Akash"
+let islogged = true;
+let state;
+
+//  type of datatype 
+
+    //   number =>2^53 range 
+    // bigint
+    // string 
+    // Boolean
+    // null
+    // undefined
+    // Symbol
+
+// object 
+   
+console.log(typeof null); // object 
+console.log(typeof undefined); // undefined  
