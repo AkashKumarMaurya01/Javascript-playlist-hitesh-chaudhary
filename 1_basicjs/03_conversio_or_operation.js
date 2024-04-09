3c3cf8b6131ce0a85c4df8387aba3d605cb57ed8
Akash kumar maurya <akashmaurya09994@gmail.com>
2024-04-09 20:21:06 +0530

    datatype and operation

--- 1_basicjs/03_conversio_or_operation.js
+++ 1_basicjs/03_conversio_or_operation.js
@@ -51,18 +51,18 @@
 */
 
 // for example 
-let temp1 = 1
-let temp2 = "Akm"
-let temp3 = ""
+// let temp1 = 1
+// let temp2 = "Akm"
+// let temp3 = ""
 
-let val1 = Boolean(temp1)
-let val2 = Boolean(temp2)
-let val3 = Boolean(temp3)
+// let val1 = Boolean(temp1)
+// let val2 = Boolean(temp2)
+// let val3 = Boolean(temp3)
 
 
-console.log(typeof val1);
-console.log(typeof val2);
-console.log(typeof val3);
+// console.log(typeof val1);
+// console.log(typeof val2);
+// console.log(typeof val3);
 
 // output of datatype
     // boolean
@@ -70,22 +70,83 @@ console.log(typeof val3);
     // boolean
 
 
-console.log(val1);
-console.log(val2);
-console.log(val3);
+// console.log(val1);
+// console.log(val2);
+// console.log(val3);
 
 // value output of each conversed variable 
     // true
     // true
     // false
 
-let somenumber = 33
+// let somenumber = 33
 
-let str = String(somenumber)
+// let str = String(somenumber)
 
-console.log(str);
-console.log(typeof str)
+// console.log(str);
+// console.log(typeof str)
 
 //  output of number to string conversion
 // 33
 // string
+
+
+
+
+
+
+
+
+
+// ************************ Operation *********
+
+
+// let str1 = "Akash"
+// let str2 = " kumar"
+
+// let str3  = str1+str2
+
+// console.log(str3);
+
+// some other operation are like 
+
+    //    console.log(2+2);
+    //    console.log(2-2);
+    //    console.log(2*2);
+    //    console.log(2**5); // this is act as 2 the power of 5 => 32
+    //    console.log(2/5);
+    //    console.log(3%2);
+
+// some point to know 
+
+    // console.log(1 + "2");  // 12
+    // console.log("1" + 1);  // 11
+    // console.log("1" + 1 + 2); // 112
+    // console.log(2 + 1 + "2");  // 32
+
+// **NOte 
+
+    // agr string aur number add kar rhe hai to 
+    // agr number phle aaya to operation number par hoga 
+    // agr string aya to normally concationate ho jayega 
+   
+
+
+
+// increment operator 
+
+// int count = 100;
+// count++;
+// console.log(count);
+
+// output  : 101 
+   
+   
+
+
+
+
+
+
+
+ 
