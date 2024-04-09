babca5b705559d9707c206b2591cca0a30a404e6
Akash kumar maurya <akashmaurya09994@gmail.com>
2024-04-09 19:51:21 +0530

    Coversion of data type

---
+++ 1_basicjs/03_conversio_or_operation.js
@@ -0,0 +1,91 @@
+// declaring variable 
+
+    // let score1 = "23";
+    // let score2 = "23Abs"
+    // let score3 = null 
+    // let score4 = undefined
+    // let score5 = false
+
+// here we are converting the data type in number 
+
+    // let conversed1 =Number(score1)
+    // let conversed2 =Number(score2)
+    // let conversed3 =Number(score3)
+    // let conversed4 =Number(score4)
+    // let conversed5 =Number(score5)
+
+// datatype of each variable
+
+    // console.log( typeof conversed1);
+    // console.log( typeof conversed2);
+    // console.log( typeof conversed3);
+    // console.log( typeof conversed4);
+
+// output of each number
+    // number
+    // number
+    // number
+    // number
+    // number
+
+// printing the value of each of the variable 
+    // console.log( conversed1);
+    // console.log( conversed2);
+    // console.log( conversed3);
+    // console.log( conversed4);
+    // console.log( conversed5);
+
+// output of each variables 
+
+    // "23"=>      23
+    // "23Abs"=>   NaN
+    // null=>      0
+    // undefined=> NaN
+    // false =>    0
+
+// Notes that (:=> Nan stand for Not A number
+
+/*
+  Same as it we use to change the data type of diffrent data type 
+  like number to boolean or string 
+*/
+
+// for example 
+let temp1 = 1
+let temp2 = "Akm"
+let temp3 = ""
+
+let val1 = Boolean(temp1)
+let val2 = Boolean(temp2)
+let val3 = Boolean(temp3)
+
+
+console.log(typeof val1);
+console.log(typeof val2);
+console.log(typeof val3);
+
+// output of datatype
+    // boolean
+    // boolean
+    // boolean
+
+
+console.log(val1);
+console.log(val2);
+console.log(val3);
+
+// value output of each conversed variable 
+    // true
+    // true
+    // false
+
+let somenumber = 33
+
+let str = String(somenumber)
+
+console.log(str);
+console.log(typeof str)
+
+//  output of number to string conversion
+// 33
+// string
