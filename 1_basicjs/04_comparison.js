4f670dfe624c62abc4cf4e7c5a6a0f688bf8edc3
Akash kumar maurya <akashmaurya09994@gmail.com>
2024-04-11 20:34:23 +0530

    comaparison of data types

---
+++ 1_basicjs/04_comparison.js
@@ -0,0 +1,38 @@
+//  Comparison 
+    // console.log(2>1);
+    // console.log(2>=1);
+    // console.log(2<1);
+//  output 
+    // true 
+    // true 
+    // false
+
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+
+// output 
+    // false
+    // false
+    // true
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// output
+
+    // true
+    // true
+
+
+//  yaha par == equality operator alg tarike se kam karta h ... 
+// baki null other > ,  >=  , < me null 0 ki trah treat hota h 
+
+console.log(undefined==0)
+console.log(undefined>0)
+console.log(undefined<0)
+
+console.log("2"===2);
+
+// in all these cases undefined compared with any operator to value it gives undefined
