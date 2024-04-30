fb4ae9f4c7cde7911fc50e0cfb0375dd8af2c1cc
Akash kumar maurya <akashmaurya09994@gmail.com>
2024-04-30 16:30:10 +0530

    Objects part 2

---
+++ 2_basic javascript/4_objects.js
@@ -0,0 +1,58 @@
+// Singleton object 
+
+const obj1 = new Object(); // this is singleton object
+
+const instauser ={}       // this is non singlton object
+
+instauser.id = "21bcs123"
+instauser.name ="Akash"
+instauser.email ="akashmaurya09994"
+
+// console.log(instauser) //{ id: '21bcs123', name: 'Akash', email: 'akashmaurya09994' }
+
+//  nested object
+
+const whatapp = {
+    email:"akashmaurya@gmail.com",
+
+    facebook:{
+        userdetail:{
+                firstname:"Akash",
+                lastname:"maurya"
+        }
+    
+    },
+    age:21,
+}
+// console.log(whatapp.facebook.userdetail.firstname);
+
+// Object concatination
+
+const ob1 = {1:"a" , 2:"b" }
+const ob2 = {3:"a" , 4:"b" }
+const ob3 = {5:"a" , 6:"b" }
+
+// const obfinal = {ob1 , ob2 , ob3} // this is not sutaible
+// const ob_merge = Object.assign({} , ob1 ,ob2, ob3)
+// console.log(obfinal);
+// console.log(ob_merge);
+
+
+const obj_merge = {...ob1,...ob2 ,...ob3}
+console.log(obj_merge); //{ '1': 'a', '2': 'b', '3': 'a', '4': 'b', '5': 'a', '6': 'b' }
+
+console.log(Object.keys(instauser));  //  [ 'id', 'name', 'email' ]
+console.log(Object.values(instauser));//[ '21bcs123', 'Akash', 'akashmaurya09994' ]
+console.log(Object.entries(instauser));
+
+/*
+[
+    [ 'id', '21bcs123' ],
+    [ 'name', 'Akash' ],
+    [ 'email', 'akashmaurya09994' ]
+  ]
+
+  all we can notice that the data type of the all key values and entries are of array type
+  */
+
+  console.log(instauser.hasOwnProperty('name')); // true if the key is present or not 
