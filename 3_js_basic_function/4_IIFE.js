7ab6827fe39e5d6e0500169946edc6ce8bff3855
Akash kumar maurya <akashmaurya09994@gmail.com>
2024-04-30 21:54:58 +0530

    IIFE in js

---
+++ 3_js_basic_function/4_IIFE.js
@@ -0,0 +1,20 @@
+//Immediately Invoked Function Expression (IIFE)
+
+// new way of calling immediately function : taki hmara local aur global variable pollution na kar sake
+(function chai() // name IIFE
+{
+    console.log(`DB connected`);
+})(); // for ending the function we should use the terminator 
+
+(()=>
+{
+    console.log("db connected two");
+})() ;
+
+
+((name)=>
+{
+    console.log(`db connected two ${name}`);
+})("Akash") ;
+
+
