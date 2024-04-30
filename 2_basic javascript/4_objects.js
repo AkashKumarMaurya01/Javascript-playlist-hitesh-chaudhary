79ceaf98ca218d5f5d3cb2dbcfb4ebb7213c7057
Akash kumar maurya <akashmaurya09994@gmail.com>
2024-04-30 17:10:02 +0530

    json

--- 2_basic javascript/4_objects.js
+++ 2_basic javascript/4_objects.js
@@ -39,11 +39,11 @@ const ob3 = {5:"a" , 6:"b" }
 
 
 const obj_merge = {...ob1,...ob2 ,...ob3}
-console.log(obj_merge); //{ '1': 'a', '2': 'b', '3': 'a', '4': 'b', '5': 'a', '6': 'b' }
+// console.log(obj_merge); //{ '1': 'a', '2': 'b', '3': 'a', '4': 'b', '5': 'a', '6': 'b' }
 
-console.log(Object.keys(instauser));  //  [ 'id', 'name', 'email' ]
-console.log(Object.values(instauser));//[ '21bcs123', 'Akash', 'akashmaurya09994' ]
-console.log(Object.entries(instauser));
+// console.log(Object.keys(instauser));  //  [ 'id', 'name', 'email' ]
+// console.log(Object.values(instauser));//[ '21bcs123', 'Akash', 'akashmaurya09994' ]
+// console.log(Object.entries(instauser));
 
 /*
 [
@@ -55,4 +55,32 @@ console.log(Object.entries(instauser));
   all we can notice that the data type of the all key values and entries are of array type
   */
 
-  console.log(instauser.hasOwnProperty('name')); // true if the key is present or not 
+//   console.log(instauser.hasOwnProperty('name')); // true if the key is present or not 
+
+
+const course = {
+    coursename :"js in hindi",
+    price :999,
+    courserInstructor:"histesh",
+}
+console.log(course.price);
+
+console.log(course.courserInstructor); // every time if we are using this notation the code become little bulky and most top coder use the below method
+
+const {courserInstructor:instructor} = course // this is the short method of using the object entities
+console.log(instructor);
+
+/*
+ Json code intro only
+{
+    "name":"Akash",
+    "coursename":"js in hindi",
+    "price":"free"
+}
+
+*/
+
+
+
+
+
