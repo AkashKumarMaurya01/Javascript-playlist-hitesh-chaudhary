0477a0768b172eead43f36261ac365547b005f44
Akash kumar maurya <akashmaurya09994@gmail.com>
2024-05-01 17:03:09 +0530

    switch control flow

---
+++ 4_control_flow/2_switch_control.js
@@ -0,0 +1,20 @@
+// Switch case 
+const month = 'April'
+switch (month) {
+    case 'Jan':
+        console.log("Janauary");
+        break;
+    case 'Feb':
+        console.log("Febuary");
+        break;
+    case 'March':
+        console.log("March");
+        break;
+    case 'April':
+        console.log("April");
+        break;
+
+    default:
+        console.log("the default case");
+        break;
+}
