63caa2058ab5cab57805a640e2754c3e8e8230bd
Akash kumar maurya <akashmaurya09994@gmail.com>
2024-05-16 17:09:06 +0530

    for loop

---
+++ 5_iterator/1_iterator.js
@@ -0,0 +1,57 @@
+// for loop
+/*
+for (let index = 0; index < 10; index++) {
+    const element = index;
+
+    if(index==5 )
+    {
+        console.log("5 is best number");
+    }
+    console.log(index);
+
+}
+*/
+
+
+// nested loop 
+/*
+for (let i = 0; i <=10; i++) {
+    
+    for (let j = 0; j <=10; j++) {
+       console.log(`i = ${i} and j =${j}`);
+    }
+    
+}
+*/
+
+
+/*
+const myarray = ["Array" , "new " ,"define"]
+
+for (let i = 0; i < myarray.length; i++) 
+{
+    const element = myarray[i];
+    console.log(element);
+    
+}
+*/
+
+// break 
+for (let i = 1; i <= 20; i++) {
+    if(i ==5){
+        console.log("5 is detected break");
+        break
+    }
+    console.log(`vlaue of i is ${i}`);
+}
+// continue 
+
+for (let j = 1; j <=20; j++) {
+    
+    if(j==5)
+    {
+        console.log("5 is detected continue");
+        continue
+    }
+    console.log(`the value of j is ${j}`);
+}
