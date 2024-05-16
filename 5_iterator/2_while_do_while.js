6a62dc6a0ccd863d80814ffd05d3004182aa0845
Akash kumar maurya <akashmaurya09994@gmail.com>
2024-05-16 17:09:50 +0530

    whille loop

---
+++ 5_iterator/2_while_do_while.js
@@ -0,0 +1,20 @@
+// while loop 
+/*
+let i =0;
+
+while(i <= 10)
+{
+    console.log(`the value of i is ${i}`);
+    i = i+2;
+}
+*/
+
+// do while loop- 
+
+let score = 1;
+do
+{
+    console.log(`score is ${score}`)
+    score++;
+}
+while(score<=5)
